7d11b0d37aa4fe2997f8dd93d3f1c2446a09f852
Gabriel GT <gabrielavila04@protonmail.com>
2023-05-28 21:03:48 -0300

    build: :monocle_face: try returning nothing

--- src/Enabled.tsx
+++ src/Enabled.tsx
@@ -23,85 +23,85 @@ const animations = {
 function Enabled() {
   console.log("!@#");
   debugger;
+  return null;
+  // const handlePreload = useCallback(async () => {
+  //   const { load } = stateStorage.get("scene");
 
-  const handlePreload = useCallback(async () => {
-    const { load } = stateStorage.get("scene");
+  //   // it shall be in the public folder!!
+  //   const ambient = load.preload("ambient", "/assets/glb/mario_level_1.glb");
 
-    // it shall be in the public folder!!
-    const ambient = load.preload("ambient", "/assets/glb/mario_level_1.glb");
+  //   // it shall be in the public folder!!
+  //   const character = load.preload("mario", "/assets/glb/mario-t-pose.glb");
 
-    // it shall be in the public folder!!
-    const character = load.preload("mario", "/assets/glb/mario-t-pose.glb");
+  //   load.preload("mario-idle", "/assets/glb/mario.glb");
 
-    load.preload("mario-idle", "/assets/glb/mario.glb");
+  //   load.preload("mario-walking", "/assets/glb/mario-walking.glb");
 
-    load.preload("mario-walking", "/assets/glb/mario-walking.glb");
+  //   load.preload("mario-running", "/assets/glb/mario-running.glb");
 
-    load.preload("mario-running", "/assets/glb/mario-running.glb");
+  //   load.preload(
+  //     "mario-walking_backwards",
+  //     "/assets/glb/mario-walking_backwards.glb"
+  //   );
 
-    load.preload(
-      "mario-walking_backwards",
-      "/assets/glb/mario-walking_backwards.glb"
-    );
+  //   load.preload(
+  //     "mario-running_backwards",
+  //     "/assets/glb/mario-running_backwards.glb"
+  //   );
 
-    load.preload(
-      "mario-running_backwards",
-      "/assets/glb/mario-running_backwards.glb"
-    );
+  //   load.preload("mario-walking_left", "/assets/glb/mario-walking_left.glb");
 
-    load.preload("mario-walking_left", "/assets/glb/mario-walking_left.glb");
+  //   load.preload("mario-walking_right", "/assets/glb/mario-walking_right.glb");
 
-    load.preload("mario-walking_right", "/assets/glb/mario-walking_right.glb");
+  //   load.preload("mario-running_left", "/assets/glb/mario-running_left.glb");
 
-    load.preload("mario-running_left", "/assets/glb/mario-running_left.glb");
+  //   load.preload("mario-running_right", "/assets/glb/mario-running_right.glb");
 
-    load.preload("mario-running_right", "/assets/glb/mario-running_right.glb");
+  //   await Promise.all([ambient, character]);
+  // }, []);
 
-    await Promise.all([ambient, character]);
-  }, []);
+  // useEffect(() => {
+  //   const handleListener = () => {
+  //     if (stateStorage.get("is_playing")) return;
 
-  useEffect(() => {
-    const handleListener = () => {
-      if (stateStorage.get("is_playing")) return;
+  //     // add song
+  //     const song = new Audio("/assets/mp3/theme_song.mp3");
 
-      // add song
-      const song = new Audio("/assets/mp3/theme_song.mp3");
+  //     // make it volume to 0.25
+  //     song.volume = 0.25;
 
-      // make it volume to 0.25
-      song.volume = 0.25;
+  //     song.loop = true;
+  //     song
+  //       .play()
+  //       .then(() => {
+  //         stateStorage.set("is_playing", true);
+  //         console.log("success");
+  //       })
+  //       .catch((err) => console.log(err));
+  //   };
 
-      song.loop = true;
-      song
-        .play()
-        .then(() => {
-          stateStorage.set("is_playing", true);
-          console.log("success");
-        })
-        .catch((err) => console.log(err));
-    };
+  //   // when there's a mouse move, add song
+  //   document.addEventListener("mousemove", handleListener);
 
-    // when there's a mouse move, add song
-    document.addEventListener("mousemove", handleListener);
+  //   return () => {
+  //     document.removeEventListener("mousemove", handleListener);
+  //   };
+  // }, []);
 
-    return () => {
-      document.removeEventListener("mousemove", handleListener);
-    };
-  }, []);
-
-  return (
-    <div>
-      <Enable3d>
-        <Initial />
-        <Preload onPreload={handlePreload} />
-        <Lights />
-        <Camera />
-        <Ambient />
-        <Character name="main" asset="mario" isMainCharacter />
-        {/* <Character name="test" asset="cat" /> */}
-        <Controls />
-      </Enable3d>
-    </div>
-  );
+  // return (
+  //   <div>
+  //     <Enable3d>
+  //       <Initial />
+  //       <Preload onPreload={handlePreload} />
+  //       <Lights />
+  //       <Camera />
+  //       <Ambient />
+  //       <Character name="main" asset="mario" isMainCharacter />
+  //       {/* <Character name="test" asset="cat" /> */}
+  //       <Controls />
+  //     </Enable3d>
+  //   </div>
+  // );
 }
 
 export default Enabled;
