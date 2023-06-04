43c6ac0fbb6a601a88da76c28324a7df4013a794
Gabriel GT <gabrielavila04@protonmail.com>
2023-06-03 21:25:29 -0300

    feat: :sparkles: improve rotation for mobile devices

--- src/components/Controls/Controls.tsx
+++ src/components/Controls/Controls.tsx
@@ -57,7 +57,7 @@ const Controls = memo(({ onUpdate }: IControl) => {
       character.body.setAngularVelocityY(0);
 
       const l = Math.abs(theta - thetaMan);
-      let rotationSpeed = isTouchDevice != null ? 2 : 4;
+      let rotationSpeed = isTouchDevice != null ? 6 : 4;
       const d = Math.PI / 24;
 
       if (l > d) {
